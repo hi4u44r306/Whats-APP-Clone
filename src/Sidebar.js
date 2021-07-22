401ced71eb6f52b81a6b341e5a5bbf47a286013b
Victor <hi4u44r306@gmail.com>
2021-07-22 11:02:22 +0800

    Start Whats App Clone

---
+++ src/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Sidebar.css';
+import { Avatar } from '@material-ui/core';
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                
+            </div>
+
+            <div className="sidebar__search">
+
+            </div>
+
+            <div className="sidebar__chats">
+
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAntY49yh3r4MFt61vkyhI3-5IcEM0eUDM",
+    authDomain: "whats-app-clone-7c728.firebaseapp.com",
+    projectId: "whats-app-clone-7c728",
+    storageBucket: "whats-app-clone-7c728.appspot.com",
+    messagingSenderId: "809849279965",
+    appId: "1:809849279965:web:c5891441db67cee3989234",
+    measurementId: "G-JY19FF57WX"
+  };
